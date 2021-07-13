92a7270a3d2c426ee179b5ea911dd9faf0b969b7
SergeyUlyanovAQA <bigbarabumchic@mail.ru>
2021-07-13 11:28:18 +0400

    finish

--- src/main/java/guru/qa/Country.java
+++ src/main/java/guru/qa/Country.java
@@ -4,9 +4,23 @@ public class Country {
 
     String name;
     String capital;
-    boolean visited;
     int population;
+    boolean visited;
+
+    public Country() {
+    }
+
+    public Country(String name, String capital, int population, boolean visited) {
+        this.name = name;
+        this.capital = capital;
+        this.population = population;
+        this.visited = visited;
+    }
 
 
+    public String getInfoCountry() {
+        return ("Country name: " + this.name + ", Country capital: " + this.capital +
+                ", Country population: " + this.population + ", Country visited: " + this.visited);
+    }
 
-}
+ }
--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Country Indonesia = new Country();
+
+        System.out.println(Indonesia.getInfoCountry());
 
     }
 
